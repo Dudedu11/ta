7c3bd2b946a8d31924248d23b2edcbafac63125a
JamilahHandini <jamilahhandini8@gmail.com>
2022-05-15 21:46:22 +0700

    home

---
+++ fix/home.php
@@ -0,0 +1,122 @@
+<!DOCTYPE>
+<html>
+
+<div class="main_content_iner default_main_contaner_iner">
+<div class="container-fluid p-0 ">
+
+<div class="row ">
+<div class="col-12">
+<h3 class="text-center f_s_30 f_w_700 text_color_6">Selamat Datang di Sistem Inventaris</h3>
+<h3 class="text-center f_s_30 f_w_700 text_color_6">Hotel Anara</h3>
+</div>
+</div>
+<div class="row ">
+<div class="col-12">
+<div class="white_card card_height_200 mb_30">
+</div>
+</div>
+<div class="row ">
+<div class="col-lg-4">
+<div class="white_card card_height_100 default_border_1px mb_20">
+<div class="white_card_header">
+<div class="box_header m-0">
+<div class="main-title">
+<h3 class="m-0">Input Data Barang</h3>
+ </div>
+<div class="header_more_tool">
+<div class="dropdown">
+</div>
+</div>
+</div>
+</div>
+<div class="white_card_body">
+<div class = "row">
+<div class= "col-lg-4"><img class="img-fluid" width="50" height="350" src="img/icon_form.jpg"></div>
+<p class="col-lg-8">Memasukkan data barang berupa kode barang, nama barang, merk, jenis, dan kondisi.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="white_card card_height_100 default_border_1px mb_20">
+<div class="white_card_header">
+<div class="box_header m-0">
+<div class="main-title">
+<h3 class="m-0">View Data Barang</h3>
+</div>
+<div class="header_more_tool">
+<div class="dropdown">
+</div>
+</div>
+</div>
+</div>
+<div class="white_card_body">
+<div class = "row">
+<div class= "col-lg-4"><img class="img-fluid" width="50" height="350" src="img/icon_table.jpg"></div>
+<p class="col-lg-8">Digunakan untuk melihat data barang berupa kode barang, nama barang, merk, jenis, dan kondisi.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="white_card card_height_100 default_border_1px mb_20">
+<div class="white_card_header">
+<div class="box_header m-0">
+<div class="main-title">
+<h3 class="m-0">Kondisi Barang</h3>
+</div>
+<div class="float-lg-right float-none sales_renew_btns justify-content-end">
+</div>
+</div>
+</div>
+<div class="white_card_body">
+<div class = "row">
+<div class = "col-lg-4"><img class="img-fluid" width="50" height="350" src="img/icon_filter.jpg"></div>
+<p class="col-lg-8">Berfungsi untuk mengetahui kondisi dari barang yang bersangkutan.</p>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class = "row">
+<div class="col-lg-12">
+<div class="white_card card_height_100 default_border_1px mb_20">
+<div class="white_card_header">
+<div class="box_header m-0">
+<div class="main-title">
+
+</div>
+<div class="float-lg-right float-none sales_renew_btns justify-content-end">
+</div>
+</div>
+</div>
+<div class="white_card_body">
+<div class = "row">
+<h5 class="text-center  text_color_6">HELP YOU TO MANAGE YOUR DATA</h5>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class = "row">
+<div class="col-lg-12">
+<div class="white_card card_height_100  mb_20">
+<div class="white_card_header">
+<div class="box_header m-0">
+<div class="main-title">
+</div>
+<div class="float-lg-right float-none sales_renew_btns justify-content-end">
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</html>
