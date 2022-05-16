cc62252e2cf4047cb2884d2a835a8bf1636d7a0d
fitrifauziyah <fitrifauziyah15@gmail.com>
2022-05-16 11:36:08 +0700

    auto refresh for input

--- fix/input_barang.php
+++ fix/input_barang.php
@@ -18,7 +18,7 @@ if(isset($_POST['submit'])){
     
     $insert = mysqli_query($conn,"INSERT INTO data_barang VALUES ('$kodeBarang', '$nama', '$tahun','$merek','$jenis','$kondisi')");
     if($insert){
-        header("location: input_data.php");
+        ?><meta http-equiv="refresh" content="0;URL='index.php?page=input_barang'"><?php
     }else{
         echo '<center>Gagal Upload</center>';
     }
@@ -27,8 +27,6 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="zxx">
-
-
 
 <div class="main_content_iner ">
 <div class="container-fluid p-0 sm_padding_15px">
@@ -148,51 +146,6 @@ Rusak Berat
 </section>
 
 
-<script src="js/jquery1-3.4.1.min.js"></script>
-
-<script src="js/popper1.min.js"></script>
-
-<script src="js/bootstrap.min.html"></script>
-
-<script src="js/metisMenu.js"></script>
-
-<script src="vendors/count_up/jquery.waypoints.min.js"></script>
-
-<script src="vendors/chartlist/Chart.min.js"></script>
-
-<script src="vendors/count_up/jquery.counterup.min.js"></script>
-
-<script src="vendors/niceselect/js/jquery.nice-select.min.js"></script>
-
-<script src="vendors/owl_carousel/js/owl.carousel.min.js"></script>
-
-<script src="vendors/datatable/js/jquery.dataTables.min.js"></script>
-<script src="vendors/datatable/js/dataTables.responsive.min.js"></script>
-<script src="vendors/datatable/js/dataTables.buttons.min.js"></script>
-<script src="vendors/datatable/js/buttons.flash.min.js"></script>
-<script src="vendors/datatable/js/jszip.min.js"></script>
-<script src="vendors/datatable/js/pdfmake.min.js"></script>
-<script src="vendors/datatable/js/vfs_fonts.js"></script>
-<script src="vendors/datatable/js/buttons.html5.min.js"></script>
-<script src="vendors/datatable/js/buttons.print.min.js"></script>
-<script src="js/chart.min.js"></script>
-
-<script src="vendors/progressbar/jquery.barfiller.js"></script>
-
-<script src="vendors/tagsinput/tagsinput.js"></script>
-
-<script src="vendors/text_editor/summernote-bs4.js"></script>
-<script src="vendors/am_chart/amcharts.js"></script>
-
-<script src="vendors/scroll/perfect-scrollbar.min.js"></script>
-<script src="vendors/scroll/scrollable-custom.js"></script>
-<script src="vendors/chart_am/core.js"></script>
-<script src="vendors/chart_am/charts.js"></script>
-<script src="vendors/chart_am/animated.js"></script>
-<script src="vendors/chart_am/kelly.js"></script>
-<script src="vendors/chart_am/chart-custom.js"></script>
-
-<script src="js/custom.js"></script>
 
 </body>
 </html>
--- fix/input_merek.php
+++ fix/input_merek.php
@@ -1,24 +1,19 @@
 <?php
 include 'connection.php';
 
-$query = mysqli_query($conn, "SELECT max(id_barang) as kodeTerbesar FROM data_barang");
+$query = mysqli_query($conn, "SELECT max(id_merk) as kodeTerbesar FROM merk");
 $data = mysqli_fetch_array($query);
-$kodeBarang = $data['kodeTerbesar'];
-$urutan = (int) substr($kodeBarang, 3, 3);
+$kodeMerek = $data['kodeTerbesar'];
+$urutan = (int) substr($kodeMerek, 3, 3);
 $urutan++;
-$huruf = "BRG";
-$kodeBarang = $huruf . sprintf("%03s", $urutan);
+$huruf = "MRK";
+$kodeMerek = $huruf . sprintf("%03s", $urutan);
 
 if(isset($_POST['submit'])){
-    $nama = $_POST['nama'];
-    $jenis = $_POST['id_jenis'];
-    $merek = $_POST['id_merek'];
-    $tahun = $_POST['tahun'];
-    $kondisi = $_POST['kondisi'];
-    
-    $insert = mysqli_query($conn,"INSERT INTO data_barang VALUES ('$kodeBarang', '$nama', '$tahun','$merek','$jenis','$kondisi')");
+    $namaMerek = $_POST['nama_merek'];
+    $insert = mysqli_query($conn,"INSERT INTO merk VALUES ('$kodeMerek', '$namaMerek')");
     if($insert){
-        header("location: input_data.php");
+        ?><meta http-equiv="refresh" content="0;URL='index.php?page=input_merek'"><?php
     }else{
         echo '<center>Gagal Upload</center>';
     }
@@ -48,81 +43,16 @@ if(isset($_POST['submit'])){
 <div class="mb-3 row">
 <label for="inputID" class="form-label col-sm-2 col-form-label">ID</label>
 <div class="col-sm-8">
-<input type="email" class="form-control" id="inputID" value="<?php echo $kodeBarang ?>" readonly>
+<input type="email" class="form-control" id="inputID" value="<?php echo $kodeMerek ?>" readonly>
 </div>
 </div>
 
 <div class="mb-3 row">
-<label for="inputNamaBarang" class="form-label col-sm-2 col-form-label">Nama</label>
+<label for="inputNamaBarang" class="form-label col-sm-2 col-form-label">Nama Merek</label>
 <div class="col-sm-8">
-<input type="type" name="nama" class="form-control" id="inputNamaBarang" placeholder="ex. Eigon Office Desk">
+<input type="type" name="nama_merek" class="form-control" id="inputNamaBarang" placeholder="ex. Eigon">
 </div>
 </div>
-
-<div class="mb-3 row">
-<label class="form-label col-sm-2 col-form-label">Jenis</label>
-<div class="col-sm-8">
-<select name="id_jenis" id="inputJenis" class="form-control" required>
-<option value=""> -- Pilih Jenis -- </option>
-<?php 
-$query1="select * from jenis";
-$tampil=mysqli_query($conn, $query1) or die(mysqli_error());
-while($data=mysqli_fetch_array($tampil)) {
-?><option value="<?php echo $data['id_jenis'];?>"><?php echo $data['nama_jenis'];?></option>
-<?php } ?>
-</select>
-</div>
-</div>
-
-<div class="mb-3 row">
-<label class="form-label col-sm-2 col-form-label">Merek</label>
-<div class="col-sm-8">
-<select name="id_merek" id="inputMerek" class="form-control" required>
-<option value=""> -- Pilih Merek -- </option>
-<?php 
-$query1="select * from merk";
-$tampil=mysqli_query($conn, $query1) or die(mysqli_error());
-while($data=mysqli_fetch_array($tampil)) {
-?><option value="<?php echo $data['id_merk'];?>"><?php echo $data['nama_merk'];?></option>
-<?php } ?>
-</select>
-</div>
-</div>
-
-<div class="mb-3 row">
-<label for="inputTahun" class="form-label col-sm-2 col-form-label">Tahun Pengadaan</label>
-<div class="col-sm-8">
-<input type="number" name="tahun" min="1000" max="9999" class="form-control" id="inputTahun" placeholder="ex. 2020">
-</div>
-</div>
-
-<fieldset class="">
-<div class="row">
-<div class="col-form-label col-sm-2 pt-0">Kondisi</div>
-<div class="col-sm-8">
-<div class="form-check">
-<input class="form-check-input" type="radio" name="kondisi" id="baik" value="Baik">
-<label class="form-label form-check-label" for="gridRadios1">
-Baik
-</label>
-</div>
-<div class="form-check">
-<input class="form-check-input" type="radio" name="kondisi" id="rusak" value="Rusak">
-<label class="form-label form-check-label" for="gridRadios2">
-Rusak
-</label>
-</div>
-<div class="form-check">
-<input class="form-check-input" type="radio" name="kondisi" id="rusak berat" value="Rusak Berat">
-<label class="form-label form-check-label" for="gridRadios3">
-Rusak Berat
-</label>
-</div>
-</label>
-</div>
-</div>
-</div>
-</fieldset>
 
 <div class=" row">
 <div class="col-sm-10">
